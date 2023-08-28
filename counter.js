a1947bc4bd13a36903f5819055aa442e069de836
Maxun <trukhinmaksimof@mail.ru>
2023-08-28 20:15:28 +0700

    deploy without worker

--- counter.js
+++ counter.js
@@ -1,4 +1,4 @@
-import MyWorker from "./multiplyWorker?worker"
+//import MyWorker from "./multiplyWorker?worker"
 
 export function setupCounter(element) {
   let counter = 0
@@ -7,22 +7,7 @@ export function setupCounter(element) {
     counter = count
     element.innerHTML = `count is ${counter}`
   }
-  if (window.Worker) {
-    const multiplicationWorker = new MyWorker()
-
-    const mul = document.getElementById("mul")
-    multiplicationWorker.onmessage = function(event) {
-      console.timeEnd("Worker timer")
-      setCounter(event.data)
-    }
-
-    mul.addEventListener('click', () => {
-      console.time("Worker timer")
-      multiplicationWorker.postMessage(counter)
-    })
-  } else {
-    console.log("Worker is not supported")
-  }
+  
   
   
   element.addEventListener('click', () => setCounter(counter + 1))
